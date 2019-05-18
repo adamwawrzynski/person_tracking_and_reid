d9fd647552e7f2d5b810f3052752bc12b70db4b7
Adam Wawrzynski <adamwawrzynski@wp.pl>
2019-05-18 17:33:36 +0200

    program.py: Add python interpreter, adjust function calls
    Signed-off-by: Adam Wawrzynski <adamwawrzynski@wp.pl>

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+#!/bin/python
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
 import cv2
@@ -8,37 +9,46 @@ import os
 from scripts.collect_dataset import get_dataset
 from scripts.collect_dataset import create_dataset
 from scripts.collect_dataset import split_dataset
+from scripts.collect_dataset import label_detected_person_on_video
 from scripts.collect_dataset import check_label_video_from_yolo
 from scripts.utils import rotateImage
 import scripts.model as model
 
 
-if __name__ == '__main__':
-    # initiate the parser
-    parser = argparse.ArgumentParser()
+# initiate the parser
+parser = argparse.ArgumentParser()
 
-    # add long and short argument
-    parser.add_argument("--source",
-                    "-s",
-                    help="path to video",
-                    dest="source",
-                    required=True)
+# add long and short argument
+parser.add_argument("--source",
+                "-s",
+                help="path to video",
+                dest="source",
+                required=True)
 
-    parser.add_argument("--confidence",
-                    "-c",
-                    help="confidence level",
-                    dest="confidence",
-                    default=0.2,
-                    required=False)
+parser.add_argument("--dataset",
+                "-d",
+                help="path to dataset",
+                dest="dataset",
+                required=True)
 
-    # read arguments from the command line
-    args = parser.parse_args()
+parser.add_argument("--weight_file",
+                "-w",
+                help="path to weight file",
+                dest="weight_file",
+                required=True)
 
+# read arguments from the command line
+args = parser.parse_args()
 
-#     check_label_video_from_yolo(args.source, 'data/data.txt', start=0, scale=0.3)
-#     model.train_model("/home/adam/person_tracking_and_reid/data/dataset", 
-#             model.cnn_net2(), 
-#             'data/cnn_net2.dh5', 
-#             5, 
-#             restore=False)
-    model.check_model(args.source, model.cnn_net2(), 'data/cnn_net2.dh5', 0.4)
+model.train_model(dataset=args.dataset,
+        model=model.cifar_10_cnn((128,128,3)),
+        weights_filename=args.weight_file,
+        iterations=10,
+        epochs=1,
+        restore=False)
+
+model.check_model(source=args.source,
+        model=model.cifar_10_cnn((128,128,3)),
+        weights_filename=args.weight_file,
+        start=0,
+        threshold=0.5)
